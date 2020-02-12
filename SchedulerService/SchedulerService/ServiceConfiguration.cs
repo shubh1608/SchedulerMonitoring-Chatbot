9a29a3b8b7b210cf2f8e31e62e1d2b15c8c14bb2
Shubham Patel <spatel@carbonite.com>
2020-02-12 22:52:25 +0530

    Added windows service, using Topshelf

---
+++ SchedulerService/SchedulerService/ServiceConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SchedulerService
+{
+    public class ServiceConfiguration
+    {
+        public static void Configure()
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<Service>(service => 
+                {
+                    service.ConstructUsing(s => new Service());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                configure.RunAsLocalSystem();
+                configure.SetServiceName("SchedulerService");
+                configure.SetDisplayName("SchedulerService");
+                configure.SetDescription("Service running background jobs using scheduler.");
+            });
+        }
+    }
+}
